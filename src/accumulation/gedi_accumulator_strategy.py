c2e77a6d80b5e9418d9165f20e6066a1dfc15233
mariarzv <maria.razzv@gmail.com>
2023-08-24 16:32:23 +0800

    Implement GeDi accumulation strategy

--- src/accumulation/gedi_accumulator_strategy.py
+++ src/accumulation/gedi_accumulator_strategy.py
@@ -2,6 +2,8 @@ import numpy as np
 import open3d as o3d
 from src.accumulation.accumulation_strategy import AccumulationStrategy
 from src.utils.gedi_registration import run_point_cloud_registration_o3d
+from gedi.gedi import GeDi
+
 
 class GediAccumulatorStrategy(AccumulationStrategy):
     """Provides a strategy that concatenates point clouds 'as is'.
@@ -10,8 +12,22 @@ class GediAccumulatorStrategy(AccumulationStrategy):
     and no further transformation is required.
     """
 
-    def on_registration(self,
-                        initial_point_cloud: o3d.geometry.PointCloud,
-                        next_point_cloud: o3d.geometry.PointCloud) -> o3d.geometry.PointCloud:
+    def __init__(self):
+        config = {'dim': 32,  # descriptor output dimension - keep it 32 always
+                  'samples_per_batch': 500,  # batches to process the data on GPU
+                  'samples_per_patch_lrf': 2000,  # num. of point to process with LRF
+                  # int(4000* scale_f),
+                  # num. of points to sample for pointnet++
+                  'samples_per_patch_out': 512,
+                  'r_lrf': 1.5,  # LRF radius
+                  'fchkpt_gedi_net': 'data/chkpts/3dmatch/chkpt.tar'}  # path to checkpoint
+
+        self.__gedi = GeDi(config)
+
+    def on_merge(self,
+                 initial_point_cloud: np.ndarray[float],
+                 next_point_cloud: np.ndarray[float]) -> np.ndarray[float]:
         size = len(next_point_cloud.points)
-        return run_point_cloud_registration_o3d(initial_point_cloud, next_point_cloud, size)
+        return run_point_cloud_registration_o3d(
+            initial_point_cloud, next_point_cloud, size,
+            self.__gedi)

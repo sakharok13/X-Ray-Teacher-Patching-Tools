b88911fb510007947e59063a79eb64d6040252b5
Alexander Dadukin <hi@st235.xyz>
2023-10-24 00:30:11 +0100

    Add visualisation of 3d boxes for once

--- src/utils/visualisation_helper.py
+++ src/utils/visualisation_helper.py
@@ -2,17 +2,34 @@ import numpy as np
 import open3d as o3d
 
 
+from pyquaternion import Quaternion
+
+
 def visualise_points_cloud(points: np.ndarray,
+                           bboxes: list = [],
                            window_title: str = 'Scene visualisation'):
     """Draws a point cloud in open3d.
 
     :param points: np.ndarray[float]
         Point cloud as a mxk float numpy array.
+    :param bboxes: list[list[float]]
+        List of bounding boxes in [cx, cy, cz, dx, dy, dz, heading] format.
     :param window_title: str
         Title of the window.
     """
 
     vector3d = o3d.utility.Vector3dVector(points[:, 0:3])
     o3d_pc = o3d.geometry.PointCloud(points=vector3d)
-    o3d.visualization.draw_geometries([o3d_pc],
+
+    o3d_boxes = []
+
+    for bbox in bboxes:
+        x, y, z, dx, dy, dz, heading = bbox
+        center = (x, y, z)
+        r = Quaternion(heading, 0, 0, 1).rotation_matrix
+        size = np.array([dx, dy, dz])
+        oriented_box = o3d.geometry.OrientedBoundingBox(center, r, size)
+        o3d_boxes.append(oriented_box)
+
+    o3d.visualization.draw_geometries([o3d_pc] + o3d_boxes,
                                       window_name=window_title)
--- visualise_once.py
+++ visualise_once.py
@@ -4,11 +4,30 @@ import numpy as np
 from src.utils.visualisation_helper import visualise_points_cloud
 
 
+def find_annotations(frame_file: str,
+                     frame_descriptors: list) -> dict:
+    for frame_descriptor in frame_descriptors:
+        if frame_descriptor['frame_id'] in frame_file:
+            return frame_descriptor
+
+    raise ValueError('Cannot fine suitable annotations')
+
 def main():
     lidar_data_path = sys.argv[1]
 
+    if len(sys.argv) > 2:
+        pickle_data_path = sys.argv[2]
+        frame_descriptors = np.load(pickle_data_path, allow_pickle=True)
+        frame_descriptor = find_annotations(frame_file=lidar_data_path, frame_descriptors=frame_descriptors)
+        bboxes = frame_descriptor['annos']['boxes_3d']
+    else:
+        bboxes = []
+
+    print(bboxes)
+
     lidar_point_clouds = np.fromfile(lidar_data_path, dtype=np.float32).reshape(-1, 4).T
     visualise_points_cloud(lidar_point_clouds.T,
+                           bboxes=bboxes,
                            window_title=lidar_data_path)
 
 

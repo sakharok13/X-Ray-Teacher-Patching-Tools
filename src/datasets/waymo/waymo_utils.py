Fix quaternion calculation

src/datasets/waymo/waymo_utils.py
@@ -82,7 +82,7 @@ def get_instance_point_cloud(frame_point_cloud: np.ndarray,
     instance_point_cloud = frame_point_cloud[:, np.where(mask)[0]]
 
     identity_transformation = transform_matrix(center_xyz,
-                                               Quaternion(heading_angle, 0, 0, 1),
+                                               Quaternion(angle=heading_angle, axis=[0, 0, 1]),
                                                inverse=True)
 
     instance_point_cloud = __apply_transformation_matrix(point_cloud=instance_point_cloud,
@@ -103,7 +103,7 @@ def reapply_frame_transformation(point_cloud: np.ndarray,
     heading_angle = annotations['heading_angles'][instance_column]
 
     reverse_transformation = transform_matrix(center_xyz,
-                                              Quaternion(heading_angle, 0, 0, 1),
+                                              Quaternion(angle=heading_angle, axis=[0, 0, 1]),
                                               inverse=False)
 
     instance_point_cloud = __apply_transformation_matrix(point_cloud=point_cloud,